f26a18d4b7e787598a6840bd58f699139e6f4a53
Web-Tushar <tusharimranvip895@gmail.com>
2023-12-21 15:18:38 +0600

    project_setup

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        headerbg: '#282828',
+        sada: '#fff'
+      },
+      maxWidth: {
+        'container': '1144px',
+      },
+      theme: {
+        fontFamily: {
+          'poppins': "'Poppins', sans-serif",
+          
+        }
+    
+    }
+  }
+}
+}
