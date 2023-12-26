4236b129c57e4f534ae097152b4d173ea0b5de36
Web-Tushar <tusharimranvip895@gmail.com>
2023-12-26 13:33:58 +0600

    service_part added

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,12 @@ tailwind.config = {
       backgroundImage: {
           'banner': "linear-gradient(rgba(0, 0, 0, 0.60),rgba(0, 0, 0, 0.60)), url('images/banner_solid_pic.jpg')",
           
+          'slidone': "linear-gradient(rgba(196, 196, 196, 0.50),rgba(196, 196, 196, 0.50)), url('images/slide_one.jpg')",
+
+          'slid_two': "linear-gradient(rgba(196, 196, 196, 0.50),rgba(196, 196, 196, 0.50)), url('images/slide_two.png')",
+
+          'slid_three': "linear-gradient(rgba(196, 196, 196, 0.50),rgba(196, 196, 196, 0.50)), url('images/slide_three.png')",
+          
       }
   }
 }
