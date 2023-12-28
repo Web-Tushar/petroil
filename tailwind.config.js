76bd4f6128e6f08cd87154930269ce0256f7fb6b
Web-Tushar <tusharimranvip895@gmail.com>
2023-12-28 11:53:51 +0600

    project complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ tailwind.config = {
         sada: '#fff',
         primary: '#F40404',
         red: '#F40404',
-        compnay_bg:'#F0F0F0'
+        compnay_bg:'#F0F0F0',
+        blogbtn: 'rgba(255, 255, 255, 0.50)'
         
       },
       maxWidth: {
@@ -18,12 +19,9 @@ tailwind.config = {
       },
       backgroundImage: {
           'banner': "linear-gradient(rgba(0, 0, 0, 0.60),rgba(0, 0, 0, 0.60)), url('images/banner_solid_pic.jpg')",
-          
-          'slidone': "linear-gradient(rgba(196, 196, 196, 0.50),rgba(196, 196, 196, 0.50)), url('images/slide_one.jpg')",
 
-          'slid_two': "linear-gradient(rgba(196, 196, 196, 0.50),rgba(196, 196, 196, 0.50)), url('images/slide_two.png')",
+          'blog_bg': "linear-gradient(rgba(0, 0, 0, 0.60),rgba(0, 0, 0, 0.60)), url('images/blog1.jpg')",
 
-          'slid_three': "linear-gradient(rgba(196, 196, 196, 0.50),rgba(196, 196, 196, 0.50)), url('images/slide_three.png')",
           
       }
   }
